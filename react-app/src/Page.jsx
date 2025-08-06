0366ab5136931daf8ac97394fb094321f1fbc13a
Abdelrahman Elgendy <abdelelgendy11@gmail.com>
2025-08-05 20:31:18 -0400

    MainContent component

--- react-app/src/Page.jsx
+++ react-app/src/Page.jsx
@@ -1,17 +1,12 @@
 import reactLogo from "./assets/react.svg"
 import Header from "./Header";
+import MainContent from "./MainContent";
 function Page() {
     return (
         <>
 
             <Header/>
-            <main>
-                <h1>Reason I am excited to learn React</h1>
-                <ol>
-                    <li>React is a popular library, so I will be able to fit in with all the coolest devs out there! 😎</li>
-                    <li>I am more likely to get a job as a front end developer if I know React</li>
-                </ol>
-            </main>
+            <MainContent/>
             <footer>
                 <small>© 2024 abdel development. All rights reserved.</small>
             </footer>
