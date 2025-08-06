b83a52d4d74e9d8cee9be2f92d192edc5bd1111e
Abdelrahman Elgendy <abdelelgendy11@gmail.com>
2025-08-05 20:34:16 -0400

    Footer components

---
+++ react-app/src/Footer.jsx
@@ -0,0 +1,8 @@
+
+export default function Footer(){
+    return(
+            <footer>
+                <small>© 2024 abdel development. All rights reserved.</small>
+            </footer>
+    )
+}
--- react-app/src/Page.jsx
+++ react-app/src/Page.jsx
@@ -1,15 +1,14 @@
 import reactLogo from "./assets/react.svg"
 import Header from "./Header";
 import MainContent from "./MainContent";
+import Footer from "./Footer";
 function Page() {
     return (
         <>
 
             <Header/>
             <MainContent/>
-            <footer>
-                <small>© 2024 abdel development. All rights reserved.</small>
-            </footer>
+            <Footer/>
             </>
         );
     }
